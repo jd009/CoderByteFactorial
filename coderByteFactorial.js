4e57e2d5b3a0f23834072bcec11fce47f37709ed
John D <redline@Johns-MacBook-Air.local>
2015-09-22 01:09:12 -0500

    solution

---
+++ coderByteFactorial.js
@@ -0,0 +1,19 @@
+function factorial(number)
+{
+   if(number === 0 ||
+      number === 1)
+   {
+      return 1;
+   }
+
+   var factors = [];
+   while(number)
+   {
+      factors.push(number);
+      number--;
+   }
+   return factors.reduce(function(a,b) { return a * b });
+}
+
+var number = document.getElementById("Number").innerHTML;
+document.getElementById("Factorial").innerHTML = factorial(parseInt(number));
